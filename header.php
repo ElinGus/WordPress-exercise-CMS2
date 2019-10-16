33ac01e6c8759060d101d19e4534a554b277cb9f
ElinGus <48243380+ElinGus@users.noreply.github.com>
2019-10-16 12:10:22 +0200

    uploading theme files

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=”UTF-8” />
+      <title><?php wp_title(); ?></title>
+      <link href=”<?php echo get_stylesheet_url(); ?>” rel=”stylesheet” type=”text/css” />
+      <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+<header></header>
